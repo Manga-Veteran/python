26de5787e2cc839a27bae3e952930c5decae4770
Manga-Veteran <78516813+Manga-Veteran@users.noreply.github.com>
2022-03-19 09:38:27 -0700

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,10 @@
+monthly = float(input('how much money do u get monthly? '))
+if monthly > 500:
+    print('rich')
+
+elif monthly > 100 and monthly < 500:
+    print('average')
+
+
+else:
+    print('ok')
---
+++ 6.py
@@ -0,0 +1,4 @@
+var = 2
+
+while var != 3:
+    var =  int(input('enter an integer: '))
